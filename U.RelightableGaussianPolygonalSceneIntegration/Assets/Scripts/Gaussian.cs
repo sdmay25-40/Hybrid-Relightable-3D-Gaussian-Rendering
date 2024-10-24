c5fc73b4a05b99f1dbb39656ce2939872ef955ee
lbroglio <lukebroglio@gmail.com>
2024-10-23 23:36:54 -0500

    First iteration of the gaussian file and parser are ready. Include demo scene as well but renderer is only partially functional

--- U.RelightableGaussianPolygonalSceneIntegration/Assets/Scripts/Gaussian.cs
+++ U.RelightableGaussianPolygonalSceneIntegration/Assets/Scripts/Gaussian.cs
@@ -3,9 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 // An Object which reprsents a Gaussian in 3D space
 public class Gaussian3D{
+
+    //NOTE: I need to research if this necessary. I don't know if the Gaussian3D class can be passed
+    // to the GPU as is or if this is necessary
+    public struct PasssableGaussian3D{
+        public Vector3 pos;
+        public Matrix4x4 cov;
+        public Matrix4x4 invCov;
+        public Vector4 color;
+    }
 
     private Vector3 _pos;
 
@@ -42,7 +52,6 @@ public class Gaussian3D{
     public Gaussian3D(float x, float y, float z, float xScale, float yScale, float zScale, 
         float alpha, float beta, float gamma, float r, float g, float b, float a) 
     {   
-        // Create position vector 
         _pos = new Vector3(x, y, z);
 
         // Create scale and rotation matrices
@@ -53,11 +62,31 @@ public class Gaussian3D{
         // Create covariance matrix from scale and rotation 
         _covariance = rotMat * scaleMat * Matrix4x4.Transpose(scaleMat) * Matrix4x4.Transpose(rotMat);
 
-        // Create color
         _color = new Vector4(r, g, b, a);
         
     }
 
+    //NOTE: I need to research if this necessary. I don't know if the Gaussian3D class can be passed
+    // to the GPU as is or if this is necessary
+
+    //Return a simple struct equivalent to the Gaussian which can be passed to the GPU
+    public PasssableGaussian3D GetPassableStruct(){
+        PasssableGaussian3D pg3D = new PasssableGaussian3D();
+        pg3D.pos = _pos;
+        pg3D.cov = _covariance;
+        pg3D.invCov = _covariance.inverse;
+        pg3D.color = _color;
+
+        return pg3D;
+
+    }
+
+    public override string ToString() {
+        return "Gaussian3D:\n{\n" + 
+            "Position:\n" + _pos + "\n" +
+            "Covariance:\n" +  _covariance + "\n" +
+            "Color:\n" + _color + "\n";
+    }
 
 }
 
@@ -65,20 +94,85 @@ public class Gaussian3D{
 // Object which can be used to parse a Gaussian .ply file and return the retrieved Gaussians
 public class GaussianPlyParser 
 {
-    private string targetPath;
+    private string pathToGaussianFile;
 
     // Create a new GaussianPlyParser to read in the file at the given path
-    public GaussianPlyParser(string targetPath){
+    public GaussianPlyParser(string pathToGaussianFile){
         // Confirm that the given file exists and throw an exception if it doesn't
-        if(!File.Exists(targetPath)){
-            throw new FileNotFoundException("File located at " + targetPath + " does not exist");
+        if(!File.Exists(pathToGaussianFile)){
+            throw new FileNotFoundException("File located at " + pathToGaussianFile + " does not exist");
         }
 
-        this.targetPath = targetPath;
+        this.pathToGaussianFile = pathToGaussianFile;
     }
-    
+
     // Parse the Gaussian file this parser to pointed at
-    public Gaussian3D[] ParseGaussianFile(){
-        throw new NotImplementedException();
+    public Gaussian3D[] ReadFile(){
+
+        using FileStream fs = File.OpenRead(pathToGaussianFile);
+        using StreamReader sr = new StreamReader(fs);
+
+        // Read the header to find the number of Gaussians in the file
+        int numGaussians = -1;
+        int lineNumber = 1;
+        string line = sr.ReadLine();
+        while (line != "end header")
+        {
+            string[] splitLine = line.Split(" ");
+
+            // If this line is the start of Gaussian description section of the header determine the number of 
+            // Gaussians to read in from it
+            if (splitLine[0] == "element" && splitLine[1] == "gaussian3D")
+            {
+                numGaussians = int.Parse(splitLine[2]);
+            }
+
+            lineNumber += 1; 
+            line = sr.ReadLine();
+        }
+
+        // If the header does not contain the line `gaussian3D {number of gaussians in the file here}`
+        // Thrown an exception
+        if(numGaussians == -1){
+            throw new InvalidOperationException("Error: Failed to read in file located at " + pathToGaussianFile +
+                ". The files header does not specify a number of gaussian3d elements to read in,");
+        }
+
+        // Parse the rest of the Gaussians
+        List<Gaussian3D> readGaussians = new List<Gaussian3D>();
+
+        int gaussiansRead = 0;
+        // TODO: If other elements are specified before the gaussians skip past them.
+        while(gaussiansRead < numGaussians){
+            line = sr.ReadLine();
+            string[] splitLine = line.Split(" ");
+
+            // If this line isn't a comment
+            if(splitLine[0] != "comment"){
+                try{
+                    readGaussians.Add(new Gaussian3D(float.Parse(splitLine[0]), float.Parse(splitLine[1]), 
+                    float.Parse(splitLine[2]), float.Parse(splitLine[3]), float.Parse(splitLine[4]), 
+                    float.Parse(splitLine[5]), float.Parse(splitLine[6]), float.Parse(splitLine[7]), 
+                    float.Parse(splitLine[8]), float.Parse(splitLine[9]), float.Parse(splitLine[10]), 
+                    float.Parse(splitLine[11]), float.Parse(splitLine[12])));
+                }
+                catch(IndexOutOfRangeException e){
+                    throw new InvalidOperationException("Error: The entry on line " + lineNumber + " of " +
+                        pathToGaussianFile + "is not a valid gaussian3D. Root Cause: " + e);
+                }
+
+                gaussiansRead += 1;
+            }
+
+            lineNumber += 1;
+        }
+
+        return readGaussians.ToArray();
+    }
+
+    // Read in a Gaussian .ply file and return its results as Gaussian3D objects
+    public static Gaussian3D[] ReadGaussianFile(string pathToGaussianFile){
+        GaussianPlyParser parser = new GaussianPlyParser(pathToGaussianFile);
+        return parser.ReadFile();
     }
 }
---
+++ U.RelightableGaussianPolygonalSceneIntegration/Assets/Scripts/GaussianDemo/GaussianDisplay.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaussianDisplay : MonoBehaviour
+{
+
+    ComputeBuffer gBuffer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gaussian3D[] gaussians =  GaussianPlyParser.ReadGaussianFile("Assets/Resources/3D/SingleGaussian.ply");
+        Debug.Log(gaussians[0]);
+
+        Gaussian3D.PasssableGaussian3D[] pGaussians = new Gaussian3D.PasssableGaussian3D[gaussians.Length];
+        for(int i = 0; i < gaussians.Length; i++){
+            pGaussians[i] = gaussians[i].GetPassableStruct();
+        } 
+
+        // Add Gaussians to buffer
+        gBuffer = new ComputeBuffer(gaussians.Length, (sizeof(float) * 3) + (sizeof(float) * 16 * 2) 
+            +  (sizeof(float) * 4));
+
+        gBuffer.SetData(pGaussians);
+
+        // Set data on GPU
+        Material m = Resources.Load("Materials/GaussianDemo/GaussianDemo") as Material;
+        m.SetBuffer("gaussians", gBuffer);
+        m.SetInt("numGaussians", gaussians.Length);
+
+        gameObject.GetComponent<MeshRenderer>().material = m;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy(){
+       gBuffer.Release();
+    }
+}
