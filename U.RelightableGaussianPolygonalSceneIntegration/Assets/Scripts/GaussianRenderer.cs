b2c82416468ddb80da33266dd6cc73a233490f1f
jvandy <jacksonvan2003@gmail.com>
2024-10-28 08:47:21 -0500

    Created a dev scene to work in, wrote most of the rendering "manager", working on calculating pixel direction vectors in world space

---
+++ U.RelightableGaussianPolygonalSceneIntegration/Assets/Scripts/GaussianRenderer.cs
@@ -0,0 +1,122 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public struct PathPayload
+{
+    // when updating, ensure PathPayload in 'Shaders/utils.cginc' is updated to match
+    public Vector3 direction;
+}
+
+public class GaussianRenderer : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private ComputeShader generatePrimaryPaths;
+    [SerializeField] private ComputeShader getPathIntersections;
+    [SerializeField] private ComputeShader samplePathIntersections;
+    [SerializeField] private int pathsPerPixel = 1;
+    private RenderTexture renderTexture;
+    private CommandBuffer commandBuffer;
+    private ComputeBuffer paths;
+    private ComputeBuffer pathsEnd;
+    private ComputeBuffer pathsContinue;
+
+    private void Awake()
+    {
+        // disable camera's rendering
+        if (cam != null)
+        {
+            Debug.LogError("'GaussianRender': 'cam' reference not set to an instance of a 'Camera'.");
+        }
+        cam.clearFlags = CameraClearFlags.Nothing;
+        cam.cullingMask = 0;
+        cam.depthTextureMode = DepthTextureMode.None;
+
+        renderTexture = new RenderTexture(Screen.width, Screen.height, 0);
+        renderTexture.enableRandomWrite = true;
+        if (!renderTexture.Create())
+        {
+            Debug.LogError("'GaussianRender': Failed to create 'RenderTexture'.");
+        }
+
+        int pathCount = Screen.width * Screen.height * pathsPerPixel;
+        paths = new ComputeBuffer(pathCount ,  Marshal.SizeOf(typeof(PathPayload)));
+        pathsEnd = new ComputeBuffer(pathCount + 1, sizeof(int));
+        pathsContinue = new ComputeBuffer(pathCount + 1, sizeof(int));
+
+        commandBuffer = new CommandBuffer();
+        commandBuffer.name = "Hybrid Gaussian Raytracer";
+
+        BuildCommandBuffer();
+    }
+
+    private void Update()
+    {
+        // if camera moves
+        // cam.RemoveCommandBuffer(CameraEvent.BeforeImageEffects, commandBuffer);
+        // commandBuffer.Clear();
+        // BuildCommandBuffer();
+    }
+
+    private void OnDestroy()
+    {
+        cam.RemoveCommandBuffer(CameraEvent.BeforeImageEffects, commandBuffer);
+        if (commandBuffer != null)
+        {
+            commandBuffer.Release();
+            commandBuffer = null;
+        }
+        if (renderTexture != null)
+        {
+            renderTexture.Release();
+            renderTexture = null;
+        }
+        if (paths != null)
+        {
+            paths.Release();
+            paths = null;
+        }
+        if (pathsContinue != null)
+        {
+            pathsContinue.Release();
+            pathsContinue = null;
+        }
+        if (pathsEnd != null)
+        {
+            pathsEnd.Release();
+            pathsEnd = null;
+        }
+    }
+
+    /// <summary>
+    /// Builds and Inserts Hybrid Gaussian Render Pipeline command buffer into `CameraEvent.BeforeImageEffects`
+    /// </summary>
+    private void BuildCommandBuffer()
+    {
+        // Unity handles resource dependency-based synchronization
+
+        // gen primary paths
+        int kernelIndex = generatePrimaryPaths.FindKernel("CSMain");
+        commandBuffer.SetComputeBufferParam(generatePrimaryPaths,kernelIndex,"paths",paths);
+
+        Vector4 cameraWorldPos = new Vector4(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z, 1);
+        commandBuffer.SetComputeVectorParam(generatePrimaryPaths, "cameraWorldPos", cameraWorldPos);
+        commandBuffer.SetComputeFloatParam(generatePrimaryPaths, "tanFovHalf", Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad * 0.5f));
+        commandBuffer.SetComputeIntParam(generatePrimaryPaths, "screenWidth", Screen.width);
+        commandBuffer.SetComputeFloatParam(generatePrimaryPaths, "invScreenHeight", 1.0f / Screen.height);
+
+        float workGroupX, workGroupY;
+        workGroupX = workGroupY = 32.0f;
+        int pathCount = Screen.width * Screen.height * pathsPerPixel;
+        int threadGroupX = Mathf.CeilToInt(pathCount / workGroupX);
+        int threadGroupY = Mathf.CeilToInt(pathCount / workGroupY);
+        commandBuffer.DispatchCompute(generatePrimaryPaths, kernelIndex, threadGroupX, threadGroupY, 1);
+
+        // get path intersects
+
+        // sample path intersects
+
+        // commandBuffer.Blit(renderTexture, BuiltinRenderTextureType.CameraTarget);
+        cam.AddCommandBuffer(CameraEvent.BeforeImageEffects, commandBuffer);
+    }
+}
---
+++ U.RelightableGaussianPolygonalSceneIntegration/Assets/Shaders/utils.cginc
@@ -0,0 +1,5 @@
+struct PathPayload
+{
+    // when updating, ensure PathPayload in 'Scripts/GaussianRenderer.cs' is updated to match
+    float3 direction;
+};
